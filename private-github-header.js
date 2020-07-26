a3a5f12e3c0b3cb2448ac9e8e8b9710aca29e1c2
Greg Back <gtback@gmail.com>
2020-07-26 16:12:53 -0400

    Initial code

---
+++ private-github-header.js
@@ -0,0 +1,17 @@
+// Decide if this is a private repo by looking for a Label with the text "Private"
+function isPrivateRepo() {
+  var outlineLabels = document.querySelectorAll(
+    "span.Label.Label--outline.v-align-middle"
+  );
+  for (const label of outlineLabels) {
+    if (label.innerText == "Private") {
+      return true;
+    }
+  }
+  return false;
+}
+
+if (isPrivateRepo()) {
+  // Set the background color of the header to dark red.
+  document.querySelectorAll("header")[0].style.backgroundColor = "darkRed";
+}
